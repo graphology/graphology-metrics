c69c59626e39fd06132b7dfbc2ae1d39727161ef
Yomguithereal <guillaumeplique@gmail.com>
2020-02-24 20:38:56 +0100

    Adding missing typings

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 export {default as centrality} from './centrality';
-export {default as density} from './density.js';
-export {default as extent} from './extent.js';
-export {default as modularity} from './modularity.js';
-export {default as weightedDegree} from './weighted-degree.js';
-export {default as weightedSize} from './weighted-size.js';
+export {default as density} from './density';
+export {default as extent} from './extent';
+export {default as modularity} from './modularity';
+export {default as weightedDegree} from './weighted-degree';
+export {default as weightedSize} from './weighted-size';
