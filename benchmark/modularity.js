a0a3ffe9cde482be90dd778bd4950a68d6c7eeb6
Yomguithereal <guillaumeplique@gmail.com>
2020-03-13 17:39:41 +0100

    toFixed 4

--- benchmark/modularity.js
+++ benchmark/modularity.js
@@ -63,12 +63,12 @@ var SPARSE_Q = ((int[1] - (tot[1] * tot[1] / M2)) + (int[2] - (tot[2] * tot[2] /
 var OTHER_SPARSE_Q = ((int[1] / M2) - Math.pow(tot[1] / M2, 2)) + ((int[1] / M2) - Math.pow(tot[1] / M2, 2));
 
 console.log('S = ', S);
-console.log('Q = ', Q.toFixed(3));
+console.log('Q = ', Q.toFixed(4));
 console.log('tot1', tot[1], 'tot2', tot[2]);
 console.log('int1', int[1], 'int2', int[2]);
 console.log('ext1', ext[1], 'ext2', ext[2]);
-console.log('sparse Q = ', SPARSE_Q.toFixed(3));
-console.log('other sparse Q =', OTHER_SPARSE_Q.toFixed(3));
+console.log('sparse Q = ', SPARSE_Q.toFixed(4));
+console.log('other sparse Q =', OTHER_SPARSE_Q.toFixed(4));
 
 // 1/2m ∑ij[Aij - (di.dj / 2m)].∂(ci, cj)
 // ∑c[(∑c-internal / 2m) - (∑c-total / 2m)²]
