a2948ec978aa1d22f93858206dd5a4f718862ddc
Yomguithereal <guillaumeplique@gmail.com>
2020-03-13 19:24:07 +0100

    Clean slate with references

--- modularity.js
+++ modularity.js
@@ -2,314 +2,38 @@
  * Graphology Modularity
  * ======================
  *
- * Implementation of modularity for graphology.
+ * Implementation of network modularity for graphology.
  *
- * Self-loops are not considered at all, not in the total weights, not in the
- * computing part (remove them and it will be the same modularity score).
- */
-var defaults = require('lodash/defaultsDeep'),
-    isGraph = require('graphology-utils/is-graph'),
-    inferType = require('graphology-utils/infer-type');
-
-var DEFAULTS = {
-  attributes: {
-    community: 'community',
-    weight: 'weight'
-  },
-  weighted: true
-};
-
-/*
-We should go towards: D = 1/(4*m) * Sum[Aij - ki*kj/(2*m)] * delta(ci, cj)
-where m = sum of weight
-      Aij = weight between i & j
-      ki = weighted outdegree of i
-      kj = weighted indegree of j
-      ci = community of i
-      cj = community of j
-*/
-
-/**
- * Function returning the modularity of the given graph.
+ * Modularity is a bit of a tricky problem because there are a wide array
+ * of different definitions and implementations. The current implementation
+ * try to stay true to Newman's original definition and consider both the
+ * undirected & directed case as well as the weighted one. The current
+ * implementation should also be aligned with the Louvain algorithm's
+ * definition of the metric.
  *
- * @param  {Graph}  graph         - Target graph.
- * @param  {object} options       - Options:
- * @param  {object}   communities - Communities mapping.
- * @param  {object}   attributes  - Attribute names:
- * @param  {string}     community - Name of the community attribute.
- * @param  {string}     weight    - Name of the weight attribute.
- * @param  {boolean}  weighted    - Whether to compute weighted modularity.
- * @return {number}
+ * [Articles]
+ * M. E. J. Newman, « Modularity and community structure in networks »,
+ * Proc. Natl. Acad. Sci. USA, vol. 103, no 23,‎ 2006, p. 8577–8582
+ * https://dx.doi.org/10.1073%2Fpnas.0601602103
+ *
+ * Blondel, Vincent D., et al. « Fast unfolding of communities in large
+ * networks ». Journal of Statistical Mechanics: Theory and Experiment,
+ * vol. 2008, no 10, octobre 2008, p. P10008. DOI.org (Crossref),
+ * doi:10.1088/1742-5468/2008/10/P10008.
+ * https://arxiv.org/pdf/0803.0476.pdf
+ *
+ * Nicolas Dugué, Anthony Perez. Directed Louvain: maximizing modularity in
+ * directed networks. [Research Report] Université d’Orléans. 2015. hal-01231784
+ * https://hal.archives-ouvertes.fr/hal-01231784
  */
-function modularity(graph, options) {
-
-  // Handling errors
-  if (!isGraph(graph))
-    throw new Error('graphology-metrics/modularity: the given graph is not a valid graphology instance.');
-
-  if (graph.multi)
-    throw new Error('graphology-metrics/modularity: multi graphs are not handled.');
-
-  if (!graph.size)
-    throw new Error('graphology-metrics/modularity: the given graph has no edges.');
-
-  var type = inferType(graph);
-
-  if (type === 'mixed')
-    throw new Error('graphology-metrics/modularity: cannot compute the modularity of a true mixed graph.');
-
-  // Solving options
-  options = defaults({}, options, DEFAULTS);
-
-  var communities,
-      community,
-      internalWeight = {},
-      totalWeight = {},
-      n;
-
-  // Do we have a community mapping?
-  if (typeof options.communities === 'object') {
-    communities = options.communities;
-
-    for (n in communities) {
-      community = communities[n];
-
-      if (!(community in internalWeight)) {
-        internalWeight[community] = 0;
-        totalWeight[community] = 0;
-      }
-    }
-  }
-
-  // Else we need to extract it from the graph
-  else {
-    communities = {};
-    graph.forEachNode(function(node, attr) {
-      community = attr[options.attributes.community];
-      communities[node] = community;
-
-      if (!(community in internalWeight)) {
-        internalWeight[community] = 0;
-        totalWeight[community] = 0;
-      }
-    });
-  }
-
-  var M = 0,
-      Q = 0;
-
-  var c1, c2, w;
-
-  var getWeight = function(attr) {
-    w = 1;
-
-    if (options.weighted) {
-      w = attr[options.attributes.weight];
-
-      if (typeof w !== 'number')
-        w = 1;
-    }
-
-    return w;
-  };
-
-  // Undirected edges
-  graph.forEachUndirectedEdge(function(edge, attr, source, target) {
-
-    // We ignore self loops
-    if (source === target)
-      return;
-
-    c1 = communities[source];
-    c2 = communities[target];
-
-    if (typeof c1 === 'undefined' || typeof c2 === 'undefined')
-      throw new Error('graphology-metrics/modularity: some nodes are not in the partition.');
-
-    w = getWeight(attr);
-
-    totalWeight[c1] += w;
-    totalWeight[c2] += w;
-    M += w * 2;
-
-    if (c1 === c2)
-      internalWeight[c1] += w * 2;
-  });
-
-  // Directed edges
-  graph.forEachDirectedEdge(function(edge, attr, source, target) {
-
-    // We ignore self loops
-    if (source === target)
-      return;
-
-    c1 = communities[source];
-    c2 = communities[target];
-
-    w = getWeight(attr);
-
-    totalWeight[c1] += w;
-    M += w;
-
-    if (c1 === c2)
-      internalWeight[c1] += w;
-  });
-
-  for (community in totalWeight)
-    Q += internalWeight[community] - totalWeight[community] * totalWeight[community] / M;
-
-  return Q / M;
-}
-
-//   for (community1 in totalW)
-//     Q += ((internalW[community1] || 0) - (totalW[community1] * totalW[community1] / M));
-
-
-
-//     totalW[community1] = (totalW[community1] || 0) + weight;
-//     if (graph.undirected(edge) || !graph.hasDirectedEdge(node2, node1)) {
-//       totalW[community2] = (totalW[community2] || 0) + weight;
-//       M += 2 * weight;
-//     }
-//     else {
-//       M += weight;
-//     }
-
-//     if (!graph.hasDirectedEdge(node2, node1))
-//       weight *= 2;
-
-//     if (community1 === community2)
-//       internalW[community1] = (internalW[community1] || 0) + weight;
-//   }
-
-
-module.exports = modularity;
-
-
-// /**
-//  * Graphology Modularity
-//  * ======================
-//  *
-//  * Notes:
-//  * The following code is taken from Gephi and Gephi doesn't consider directed
-//  * edges:
-//  *
-//  * Directed edges produces the same modularity as if they were undirected
-//  *   - if there are a->b and b->a : consider a<->b
-//  *   - if there is a->b only or b->a only : consider ALSO a<->b
-//  *   - if there are a->b , b->a with differents weights, only one is considered
-//  *
-//  * The order chosen by Gephi is unknown, it is a sensitive case and is not
-//  * handled.
-//  *
-//  * Self-loops are not considered at all, not in the total weights, not in the
-//  * computing part (remove them and it will be the same modularity score).
-//  */
 // var defaults = require('lodash/defaultsDeep'),
-//     isGraph = require('graphology-utils/is-graph');
+//     isGraph = require('graphology-utils/is-graph'),
+//     inferType = require('graphology-utils/infer-type');
 
 // var DEFAULTS = {
 //   attributes: {
 //     community: 'community',
 //     weight: 'weight'
-//   }
+//   },
+//   weighted: true
 // };
-
-// /**
-//  * Function returning the modularity of the given graph.
-//  *
-//  * @param  {Graph}  graph         - Target graph.
-//  * @param  {object} options       - Options:
-//  * @param  {object}   communities - Communities mapping.
-//  * @param  {object}   attributes  - Attribute names:
-//  * @param  {string}     community - Name of the community attribute.
-//  * @param  {string}     weight    - Name of the weight attribute.
-//  * @return {number}
-//  */
-// function modularity(graph, options) {
-
-//   // Handling errors
-//   if (!isGraph(graph))
-//     throw new Error('graphology-metrics/modularity: the given graph is not a valid graphology instance.');
-
-//   if (graph.multi)
-//     throw new Error('graphology-metrics/modularity: multi graphs are not handled.');
-
-//   if (!graph.size)
-//     throw new Error('graphology-metrics/modularity: the given graph has no edges.');
-
-//   // Solving options
-//   options = defaults({}, options, DEFAULTS);
-
-//   var communities,
-//       nodes = graph.nodes(),
-//       edges = graph.edges(),
-//       i,
-//       l;
-
-//   // Do we have a community mapping?
-//   if (typeof options.communities === 'object') {
-//     communities = options.communities;
-//   }
-
-//   // Else we need to extract it from the graph
-//   else {
-//     communities = {};
-
-//     for (i = 0, l = nodes.length; i < l; i++)
-//       communities[nodes[i]] = graph.getNodeAttribute(nodes[i], options.attributes.community);
-//   }
-
-//   var M = 0,
-//       Q = 0,
-//       internalW = {},
-//       totalW = {},
-//       bounds,
-//       node1, node2, edge,
-//       community1, community2,
-//       w, weight;
-
-//   for (i = 0, l = edges.length; i < l; i++) {
-//     edge = edges[i];
-//     bounds = graph.extremities(edge);
-//     node1 = bounds[0];
-//     node2 = bounds[1];
-
-//     if (node1 === node2)
-//       continue;
-
-//     community1 = communities[node1];
-//     community2 = communities[node2];
-
-//     if (community1 === undefined)
-//       throw new Error('graphology-metrics/modularity: the "' + node1 + '" node is not in the partition.');
-
-//     if (community2 === undefined)
-//       throw new Error('graphology-metrics/modularity: the "' + node2 + '" node is not in the partition.');
-
-//     w = graph.getEdgeAttribute(edge, options.attributes.weight);
-//     weight = isNaN(w) ? 1 : w;
-
-//     totalW[community1] = (totalW[community1] || 0) + weight;
-//     if (graph.undirected(edge) || !graph.hasDirectedEdge(node2, node1)) {
-//       totalW[community2] = (totalW[community2] || 0) + weight;
-//       M += 2 * weight;
-//     }
-//     else {
-//       M += weight;
-//     }
-
-//     if (!graph.hasDirectedEdge(node2, node1))
-//       weight *= 2;
-
-//     if (community1 === community2)
-//       internalW[community1] = (internalW[community1] || 0) + weight;
-//   }
-
-//   for (community1 in totalW)
-//     Q += ((internalW[community1] || 0) - (totalW[community1] * totalW[community1] / M));
-
-//   return Q / M;
-// }
-
-// module.exports = modularity;
--- test/modularity.js
+++ test/modularity.js
@@ -2,188 +2,188 @@
  * Graphology Modularity Unit Tests
  * =================================
  */
-var assert = require('chai').assert,
-    Graph = require('graphology'),
-    modularity = require('../modularity.js');
+// var assert = require('chai').assert,
+//     Graph = require('graphology'),
+//     modularity = require('../modularity.js');
 
-/**
- * Test helpers.
- */
-var TYPE = {
-  UNDIRECTED: 1,
-  DIRECTED: 2,
-  MIXED: 3,
-};
+// /**
+//  * Test helpers.
+//  */
+// var TYPE = {
+//   UNDIRECTED: 1,
+//   DIRECTED: 2,
+//   MIXED: 3,
+// };
 
-function parse(dataset, t) {
-   var graph = new Graph(),
-      n = dataset.nodes,
-      e = dataset.edges,
-      partitioning = {},
-      i, l;
+// function parse(dataset, t) {
+//    var graph = new Graph(),
+//       n = dataset.nodes,
+//       e = dataset.edges,
+//       partitioning = {},
+//       i, l;
 
-  for (i = 0, l = n.length; i < l; i++) {
-    graph.addNode(n[i].id);
-    partitioning[n[i].id] = n[i].attributes['Modularity Class'];
-  }
+//   for (i = 0, l = n.length; i < l; i++) {
+//     graph.addNode(n[i].id);
+//     partitioning[n[i].id] = n[i].attributes['Modularity Class'];
+//   }
 
-  for (i = 0, l = e.length; i < l; i++) {
-    if (graph.hasEdge(e[i].source, e[i].target))
-      continue;
-    if (t === TYPE.DIRECTED || (t === TYPE.MIXED && e[i].attributes.Orientation === 'directed'))
-      graph.addDirectedEdge(e[i].source, e[i].target);
-    else
-      graph.addUndirectedEdge(e[i].source, e[i].target);
-  }
+//   for (i = 0, l = e.length; i < l; i++) {
+//     if (graph.hasEdge(e[i].source, e[i].target))
+//       continue;
+//     if (t === TYPE.DIRECTED || (t === TYPE.MIXED && e[i].attributes.Orientation === 'directed'))
+//       graph.addDirectedEdge(e[i].source, e[i].target);
+//     else
+//       graph.addUndirectedEdge(e[i].source, e[i].target);
+//   }
 
-  return {graph: graph, partitioning: partitioning};
-}
+//   return {graph: graph, partitioning: partitioning};
+// }
 
-/**
- * Datasets.
- */
-var clique3 = parse(require('./datasets/clique3.json'), TYPE.DIRECTED),
-    directed500 = parse(require('./datasets/directed500.json'), TYPE.DIRECTED),
-    undirected500 = parse(require('./datasets/undirected500.json'), TYPE.UNDIRECTED);
+// /**
+//  * Datasets.
+//  */
+// var clique3 = parse(require('./datasets/clique3.json'), TYPE.DIRECTED),
+//     directed500 = parse(require('./datasets/directed500.json'), TYPE.DIRECTED),
+//     undirected500 = parse(require('./datasets/undirected500.json'), TYPE.UNDIRECTED);
 
-/**
- * Actual unit tests.
- */
-describe('modularity', function() {
-  it('should throw if given graph is invalid.', function() {
-    assert.throws(function() {
-      modularity(null, []);
-    }, /graphology/);
-  });
+// /**
+//  * Actual unit tests.
+//  */
+// describe('modularity', function() {
+//   it('should throw if given graph is invalid.', function() {
+//     assert.throws(function() {
+//       modularity(null, []);
+//     }, /graphology/);
+//   });
 
-  it('should throw if given graph is multi.', function() {
-    assert.throws(function() {
-      modularity(new Graph({multi: true}), []);
-    }, /multi/);
-  });
+//   it('should throw if given graph is multi.', function() {
+//     assert.throws(function() {
+//       modularity(new Graph({multi: true}), []);
+//     }, /multi/);
+//   });
 
-  it('should throw if the given graph has no edges.', function() {
-    var graph = new Graph();
-    graph.addNode(1);
-    graph.addNode(2);
+//   it('should throw if the given graph has no edges.', function() {
+//     var graph = new Graph();
+//     graph.addNode(1);
+//     graph.addNode(2);
 
-    assert.throws(function() {
-      modularity(graph, [[1], [2]]);
-    }, /edge/);
-  });
+//     assert.throws(function() {
+//       modularity(graph, [[1], [2]]);
+//     }, /edge/);
+//   });
 
-  it('should throw if a node is not in the given partition.', function() {
-    var graph = new Graph();
-    graph.mergeUndirectedEdge(1, 2);
-    graph.mergeUndirectedEdge(1, 3);
-    graph.mergeUndirectedEdge(2, 3);
+//   it('should throw if a node is not in the given partition.', function() {
+//     var graph = new Graph();
+//     graph.mergeUndirectedEdge(1, 2);
+//     graph.mergeUndirectedEdge(1, 3);
+//     graph.mergeUndirectedEdge(2, 3);
 
-    assert.throws(function() {
-      modularity(graph, {communities: {1: 0, 2: 0}});
-    }, /partition/);
-  });
+//     assert.throws(function() {
+//       modularity(graph, {communities: {1: 0, 2: 0}});
+//     }, /partition/);
+//   });
 
-  it('should handle unique partitions of cliques.', function() {
-    var graph = new Graph();
-    graph.mergeUndirectedEdge(1, 2);
-    graph.mergeUndirectedEdge(1, 3);
-    graph.mergeUndirectedEdge(2, 3);
+//   it('should handle unique partitions of cliques.', function() {
+//     var graph = new Graph();
+//     graph.mergeUndirectedEdge(1, 2);
+//     graph.mergeUndirectedEdge(1, 3);
+//     graph.mergeUndirectedEdge(2, 3);
 
-    assert.closeTo(modularity(graph, {communities: {1: 0, 2: 0, 3: 0}}), 0, 0.01);
-  });
+//     assert.closeTo(modularity(graph, {communities: {1: 0, 2: 0, 3: 0}}), 0, 0.01);
+//   });
 
-  it('should handle tiny weighted graphs (5 nodes).', function() {
-    var graph = new Graph();
+//   it('should handle tiny weighted graphs (5 nodes).', function() {
+//     var graph = new Graph();
 
-    graph.mergeUndirectedEdge(1, 2, {weight: 30});
-    graph.mergeUndirectedEdge(1, 5);
-    graph.mergeUndirectedEdge(2, 3, {weight: 15});
-    graph.mergeUndirectedEdge(2, 4, {weight: 10});
-    graph.mergeUndirectedEdge(2, 5);
-    graph.mergeUndirectedEdge(3, 4, {weight: 5});
-    graph.mergeUndirectedEdge(4, 5, {weight: 100});
+//     graph.mergeUndirectedEdge(1, 2, {weight: 30});
+//     graph.mergeUndirectedEdge(1, 5);
+//     graph.mergeUndirectedEdge(2, 3, {weight: 15});
+//     graph.mergeUndirectedEdge(2, 4, {weight: 10});
+//     graph.mergeUndirectedEdge(2, 5);
+//     graph.mergeUndirectedEdge(3, 4, {weight: 5});
+//     graph.mergeUndirectedEdge(4, 5, {weight: 100});
 
-    assert.closeTo(modularity(graph, {communities: {1: 0, 2: 0, 3: 0, 4: 1, 5: 1}}), 0.337, 0.01);
-  });
+//     assert.closeTo(modularity(graph, {communities: {1: 0, 2: 0, 3: 0, 4: 1, 5: 1}}), 0.337, 0.01);
+//   });
 
-  it('should be possible to indicate the weight attribute name.', function() {
-    var graph = new Graph();
+//   it('should be possible to indicate the weight attribute name.', function() {
+//     var graph = new Graph();
 
-    graph.mergeUndirectedEdge(1, 2, {customWeight: 30});
-    graph.mergeUndirectedEdge(1, 5);
-    graph.mergeUndirectedEdge(2, 3, {customWeight: 15});
-    graph.mergeUndirectedEdge(2, 4, {customWeight: 10});
-    graph.mergeUndirectedEdge(2, 5);
-    graph.mergeUndirectedEdge(3, 4, {customWeight: 5});
-    graph.mergeUndirectedEdge(4, 5, {customWeight: 100});
+//     graph.mergeUndirectedEdge(1, 2, {customWeight: 30});
+//     graph.mergeUndirectedEdge(1, 5);
+//     graph.mergeUndirectedEdge(2, 3, {customWeight: 15});
+//     graph.mergeUndirectedEdge(2, 4, {customWeight: 10});
+//     graph.mergeUndirectedEdge(2, 5);
+//     graph.mergeUndirectedEdge(3, 4, {customWeight: 5});
+//     graph.mergeUndirectedEdge(4, 5, {customWeight: 100});
 
-    var options = {
-      attributes: {
-        weight: 'customWeight'
-      },
-      communities: {1: 0, 2: 0, 3: 0, 4: 1, 5: 1}
-    };
+//     var options = {
+//       attributes: {
+//         weight: 'customWeight'
+//       },
+//       communities: {1: 0, 2: 0, 3: 0, 4: 1, 5: 1}
+//     };
 
-    assert.closeTo(modularity(graph, options), 0.337, 0.01);
-  });
+//     assert.closeTo(modularity(graph, options), 0.337, 0.01);
+//   });
 
-  it('should be possible to read the communities from the graph', function() {
-    var graph = new Graph();
+//   it('should be possible to read the communities from the graph', function() {
+//     var graph = new Graph();
 
-    var data = {
-      1: {
-        community: 0
-      },
-      2: {
-        community: 0
-      },
-      3: {
-        community: 0
-      },
-      4: {
-        community: 1
-      },
-      5: {
-        community: 1
-      }
-    };
+//     var data = {
+//       1: {
+//         community: 0
+//       },
+//       2: {
+//         community: 0
+//       },
+//       3: {
+//         community: 0
+//       },
+//       4: {
+//         community: 1
+//       },
+//       5: {
+//         community: 1
+//       }
+//     };
 
-    for (var node in data)
-      graph.addNode(node, data[node]);
+//     for (var node in data)
+//       graph.addNode(node, data[node]);
 
-    graph.addUndirectedEdge(1, 2, {weight: 30});
-    graph.addUndirectedEdge(1, 5);
-    graph.addUndirectedEdge(2, 3, {weight: 15});
-    graph.addUndirectedEdge(2, 4, {weight: 10});
-    graph.addUndirectedEdge(2, 5);
-    graph.addUndirectedEdge(3, 4, {weight: 5});
-    graph.addUndirectedEdge(4, 5, {weight: 100});
+//     graph.addUndirectedEdge(1, 2, {weight: 30});
+//     graph.addUndirectedEdge(1, 5);
+//     graph.addUndirectedEdge(2, 3, {weight: 15});
+//     graph.addUndirectedEdge(2, 4, {weight: 10});
+//     graph.addUndirectedEdge(2, 5);
+//     graph.addUndirectedEdge(3, 4, {weight: 5});
+//     graph.addUndirectedEdge(4, 5, {weight: 100});
 
-    assert.closeTo(modularity(graph), 0.337, 0.01);
-  });
+//     assert.closeTo(modularity(graph), 0.337, 0.01);
+//   });
 
-  it.skip('should handle tiny directed graphs (5 nodes).', function() {
-    var graph = new Graph({type: 'directed'});
+//   it.skip('should handle tiny directed graphs (5 nodes).', function() {
+//     var graph = new Graph({type: 'directed'});
 
-    graph.mergeDirectedEdge(1, 2);
-    graph.mergeDirectedEdge(1, 5);
-    graph.mergeDirectedEdge(2, 3);
-    graph.mergeDirectedEdge(3, 4);
-    graph.mergeDirectedEdge(4, 2);
-    graph.mergeDirectedEdge(5, 1);
+//     graph.mergeDirectedEdge(1, 2);
+//     graph.mergeDirectedEdge(1, 5);
+//     graph.mergeDirectedEdge(2, 3);
+//     graph.mergeDirectedEdge(3, 4);
+//     graph.mergeDirectedEdge(4, 2);
+//     graph.mergeDirectedEdge(5, 1);
 
-    assert.closeTo(modularity(graph, {communities: {1: 0, 5: 0, 2: 1, 3: 1, 4: 1}}), 0.22, 0.01);
-  });
+//     assert.closeTo(modularity(graph, {communities: {1: 0, 5: 0, 2: 1, 3: 1, 4: 1}}), 0.22, 0.01);
+//   });
 
-  it('should handle tiny undirected graphs (12 nodes).', function() {
-    assert.closeTo(modularity(clique3.graph, {communities: clique3.partitioning}), 0.524, 0.01);
-  });
+//   it('should handle tiny undirected graphs (12 nodes).', function() {
+//     assert.closeTo(modularity(clique3.graph, {communities: clique3.partitioning}), 0.524, 0.01);
+//   });
 
-  it('should handle heavy-sized undirected graphs (500 nodes).', function() {
-    assert.closeTo(modularity(undirected500.graph, {communities: undirected500.partitioning}), 0.397, 0.01);
-  });
+//   it('should handle heavy-sized undirected graphs (500 nodes).', function() {
+//     assert.closeTo(modularity(undirected500.graph, {communities: undirected500.partitioning}), 0.397, 0.01);
+//   });
 
-  it('should handle heavy-sized directed graphs (500 nodes).', function() {
-    assert.closeTo(modularity(directed500.graph, {communities: directed500.partitioning}), 0.408, 0.01);
-  });
-});
+//   it('should handle heavy-sized directed graphs (500 nodes).', function() {
+//     assert.closeTo(modularity(directed500.graph, {communities: directed500.partitioning}), 0.408, 0.01);
+//   });
+// });
