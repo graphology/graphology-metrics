76f30549fed838184e3dd12bf61ad8ec44dd4992
Yomguithereal <guillaumeplique@gmail.com>
2020-03-13 14:57:16 +0100

    Notes & tests

---
+++ benchmark/modularity.js
@@ -0,0 +1,69 @@
+var nodes = [
+  [1, 1, 2], // id, C, deg
+  [2, 2, 3],
+  [3, 2, 2],
+  [4, 2, 2],
+  [5, 1, 1]
+];
+
+var edges = [
+  [1, 2, null], // s, t, internal?
+  [1, 5, 1],
+  [2, 3, 2],
+  [3, 4, 2],
+  [4, 2, 2],
+  [5, 1, 1]
+];
+
+var M = 5; // Undirected size (minus 1 mutual edge)
+var MM = M * 2;
+
+function hasUndirectedEdge(E, s, t) {
+  return E.some(e => {
+    return (e[0] === s && e[1] === t) || (e[1] === s && e[0] === t);
+  });
+}
+
+function hasMutualEdge(E, s, t) {
+  return hasDirectedEdge(E, s, t) && hasDirectedEdge(E, t, s);
+}
+
+function hasDirectedEdge(E, s, t) {
+  return E.some(e => {
+    return (e[0] === s && e[1] === t);
+  });
+}
+
+var S = 0, Aij, didj;
+
+var tot = {1: 0, 2: 0}, int = {1: 0, 2: 0};
+
+var i, j, l, ok;
+
+for (i = 0, l = nodes.length; i < l; i++) {
+  for (j = i + 1; j < l; j++) {
+    ok = hasUndirectedEdge(edges, nodes[i][0], nodes[j][0]);
+
+    if (ok) {
+      tot[nodes[i][1]] += 1;
+      tot[nodes[j][1]] += 1;
+    }
+
+    // Kronecker delta
+    if (nodes[i][1] !== nodes[j][1])
+      continue;
+
+    if (ok)
+      int[nodes[i][1]] += 2;
+
+    Aij = ok ? 1 : 0;
+    didj = nodes[i][2] * nodes[j][2];
+    S += Aij - (didj / (2 * M));
+  }
+}
+
+console.log('S = ', S);
+console.log('Q = ', S / (2 * M));
+console.log('tot1', tot[1], 'tot2', tot[2]);
+console.log('int1', int[1], 'int2', int[2]);
+console.log('sparse Q = ', ((int[1] - (tot[1] * tot[1] / MM)) + (int[2] - (tot[2] * tot[2] / MM))) / MM)
