67bc9df007d2a1cd175a059ed9827e6529414d9f
Yomguithereal <guillaumeplique@gmail.com>
2020-03-16 23:28:33 +0100

    Need times 2 on degree in c

--- benchmark/modularity-delta.js
+++ benchmark/modularity-delta.js
@@ -6,10 +6,14 @@ var g = new Graph.UndirectedGraph();
 g.addNode(1, {community: 1});
 g.addNode(2, {community: 1});
 g.addNode(3, {community: 2});
+g.addNode(4, {community: 1});
 
 g.addEdge(1, 2);
+g.addEdge(1, 3);
+g.addEdge(1, 4);
 g.addEdge(2, 3);
-g.addEdge(3, 1);
+g.addEdge(2, 4);
+g.addEdge(3, 4);
 
 console.log('Q =', lib.sparse(g));
 g.setNodeAttribute(3, 'community', 1);
@@ -23,11 +27,11 @@ var dic = 0,
     m = 6;
 
 // Node = 1
-// var dic = 2,
-//     di = 2,
-//     Ztot = 3,
-//     Zin = 1,
-//     m = 3;
+var dic = 3 * 2,
+    di = 3,
+    Ztot = 9,
+    Zin = 6,
+    m = 6;
 
 var DQ = dic / (2 * m) - (Ztot * di) / (2 * Math.pow(m, 2));
 
@@ -36,4 +40,4 @@ console.log(DQ.toFixed(4));
 var DDQ = ( (Zin + dic) / (2 * m) - Math.pow((Ztot + di) / (2 * m), 2) ) -
           ( Zin / (2 * m) - Math.pow(Ztot / (2 * m), 2) - Math.pow(di / (2 * m), 2));
 
-console.log(DDQ);
+console.log(DDQ.toFixed(4));
