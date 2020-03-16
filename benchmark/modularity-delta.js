b8f417f964b423c395f1c796050dcffd26e51561
Yomguithereal <guillaumeplique@gmail.com>
2020-03-16 23:34:28 +0100

    Delta display

--- benchmark/modularity-delta.js
+++ benchmark/modularity-delta.js
@@ -1,6 +1,46 @@
 var Graph = require('graphology');
 var lib = require('../modularity.js');
 
+// function fromData(G, nodes, edges) {
+//   var g = new G();
+
+//   nodes.forEach(function(node) {
+//     g.addNode(node[0], {community: node[1]});
+//   });
+
+//   edges.forEach(function(edge) {
+//     var attr = {};
+
+//     if (edge.length > 2)
+//       attr.weight = edge[2];
+
+//     g.mergeEdge(edge[0], edge[1], attr);
+//   });
+
+//   return g;
+// }
+
+// var nodes = [
+//   [1, 1], // id, community
+//   [2, 2],
+//   [3, 2],
+//   [4, 2],
+//   [5, 1],
+//   [6, 2]
+// ];
+
+// var edges = [
+//   [1, 2], // source, target
+//   [1, 5],
+//   [2, 3],
+//   [3, 4],
+//   [4, 2],
+//   [5, 1],
+//   [6, 3]
+// ];
+
+// var g = fromData(Graph.UndirectedGraph, nodes, edges);
+
 var g = new Graph.UndirectedGraph();
 
 g.addNode(1, {community: 1});
@@ -15,9 +55,11 @@ g.addEdge(2, 3);
 g.addEdge(2, 4);
 g.addEdge(3, 4);
 
-console.log('Q =', lib.sparse(g));
+var Q = lib.sparse(g);
+console.log('Q  =', Q);
 g.setNodeAttribute(3, 'community', 1);
 console.log('Q+ =', lib.sparse(g));
+console.log('∆  =', lib.sparse(g) - Q);
 
 // Node = 3
 var dic = 0,
