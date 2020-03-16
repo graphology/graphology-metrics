f2aef794edb8a732860ebc1479d5b94a14af68cd
Yomguithereal <guillaumeplique@gmail.com>
2020-03-16 15:27:31 +0100

    Better modularity typings

--- modularity.d.ts
+++ modularity.d.ts
@@ -11,4 +11,29 @@ type ModularityOptions = {
   weighted: boolean
 };
 
-export default function modularity(graph: Graph, options?: ModularityOptions): number;
+interface IModularity {
+  (graph: Graph, options?: ModularityOptions): number;
+
+  dense(graph: Graph, options?: ModularityOptions): number;
+  sparse(graph: Graph, options?: ModularityOptions): number;
+
+  undirectedDelta(
+    M: number,
+    communityTotalWeight: number,
+    nodeDegree: number,
+    nodeCommunityDegree: number
+  ): number;
+
+  directedDelta(
+    M: number,
+    communityTotalInWeight: number,
+    communityTotalOutWeight: number,
+    nodeInDegree: number,
+    nodeOutDegree: number,
+    nodeCommunityDegree: number
+  ): number;
+}
+
+declare const modularity: IModularity;
+
+export default modularity;
