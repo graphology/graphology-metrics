1e5b461974e54c682d5667288166897fd72a0c09
paubre <pauline.breteau@outlook.fr>
2020-04-21 18:08:30 +0200

    eccentricity after review

--- eccentricity.js
+++ eccentricity.js
@@ -5,43 +5,37 @@
  * Functions used to compute the eccentricity of each node of a given graph.
  */
 var isGraph = require('graphology-utils/is-graph');
-var shortestPath = require('graphology-shortest-path');
+var singleSourceLength = require('graphology-shortest-path/unweighted');
 var Graph = require('graphology');
 
 module.exports = function eccentricity(graph, mynode) {
   if (!isGraph(graph))
-    throw new Error('graphology-metrics/degree: given graph is not a valid graphology instance.');
+    throw new Error('graphology-metrics/eccentricity: given graph is not a valid graphology instance.');
 
-    var nodes = new Array(graph.order),
-    entries = new Array(graph.order),
-    i = 0, j, ecc = 0;
+  var path = {}, ecc = 0;
 
   graph.forEachNode(function(node, attr) {
-    nodes[i] = node;
-    entries[i++] = attr;
-  });
 
-  for (i = 0, l = graph.order; i < l; i++) {
-    if (nodes[i] == mynode) {
-      var path = shortestPath(graph, nodes[i])
-      var lg = path[i+1].length - 1;
+    if (node == 3){
+      path = singleSourceLength(graph, node);
 
-      if (lg > ecc) {
-        ecc = lg
+      for (i = 1, l = graph.order; i <= l; i++) {
+
+        if (path[i]) {
+          var lg = path[i].length - 1;
+
+          if (lg > ecc) {
+            ecc = lg
+          }
+        }
+        else {
+          ecc = Infinity
+        }
+
       }
     }
-  }
-
-  var ecc = 0
-
-  for (j = 1; j <= l; j++) {
-    if (path[j]) {
-      if (path[j].length - 1 > ecc)
-        ecc = path[j].length - 1;
-    }
-    else
-      ecc = Infinity;
-  }
+  });
 
   return ecc;
 }
+
