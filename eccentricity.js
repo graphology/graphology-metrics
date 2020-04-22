ae020f6e8cf5b379ccb4f24f695dae46f42d0a4b
paubre <pauline.breteau@outlook.fr>
2020-04-22 11:32:01 +0200

    eccentricity after review

--- eccentricity.js
+++ eccentricity.js
@@ -5,34 +5,31 @@
  * Functions used to compute the eccentricity of each node of a given graph.
  */
 var isGraph = require('graphology-utils/is-graph');
-var singleSourceLength = require('graphology-shortest-path/unweighted');
+var singleSourceLength = require('graphology-shortest-path/unweighted').singleSourceLength
 var Graph = require('graphology');
 
 module.exports = function eccentricity(graph, mynode) {
   if (!isGraph(graph))
     throw new Error('graphology-metrics/eccentricity: given graph is not a valid graphology instance.');
 
-  var path = {}, ecc = 0;
+  var path = {}, ecc = -Infinity;
 
   graph.forEachNode(function(node, attr) {
 
-    if (node == 3){
-      path = singleSourceLength(graph, node);
+    if (node == mynode){
+      lg = singleSourceLength(graph, node);
 
       for (i = 1, l = graph.order; i <= l; i++) {
-
-        if (path[i]) {
-          var lg = path[i].length - 1;
-
-          if (lg > ecc) {
-            ecc = lg
+        if (lg[i] != null) {
+          if (lg[i] > ecc) {
+            ecc = lg[i]
           }
         }
         else {
           ecc = Infinity
         }
-
       }
+
     }
   });
 
